9a0fad6d513036a64932917b9f02d2eb9fa67af3
Milo Silva <milo.silva234@gmail.com>
2021-01-19 10:35:44 +0100

    initialise MyParty Screen with user email input

--- src/screens/MyParty/index.js
+++ src/screens/MyParty/index.js
@@ -1,10 +1,40 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import { useDispatch } from "react-redux";
+import styled from "styled-components/native";
+import Button from "../../components/Button";
+import { inviteFriend } from "../../store/user/actions";
 
 export default function index() {
+  const [email, setEmail] = useState();
+  const dispatch = useDispatch();
+  const Title = styled.Text`
+    font-size: 26;
+    margin-bottom: 100;
+  `;
+
   return (
     <View>
-      <Text></Text>
+      <Title>Invite A Friend</Title>
+      <TextInput style={styles.input} onChangeText={setEmail} placeholder="email" />
+      <Button
+        text="dislike "
+        style={{ backgroundColor: "rgb(244, 67, 54)" }}
+        onPress={() => dispatch(inviteFriend(email))}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    width: 200,
+    height: 40,
+    borderRadius: 10,
+    backgroundColor: "#fff",
+
+    marginTop: 15,
+    paddingLeft: 10,
+  },
+});
