d42db39d2da33a4747806c47eb4305ea29527d5a
Milo Silva <milo.silva234@gmail.com>
2021-01-19 16:31:31 +0100

    configure apiUrl to use production api

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
 export const apiUrl =
   process.env.NODE_ENV === "production"
-    ? "https://my-heroku-backend-or-whatever-you-call-it.herokuapp.com"
+    ? "https://movie-match-api.herokuapp.com/"
     : "http://192.168.1.20:4000";
