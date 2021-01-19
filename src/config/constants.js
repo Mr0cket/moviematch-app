ebe941c354880b080426ac5bbfe33a9e73faf8e2
Milo Silva <milo.silva234@gmail.com>
2021-01-19 16:52:49 +0100

    reconfigure production apiUrl

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
 export const apiUrl =
   process.env.NODE_ENV === "production"
-    ? "https://movie-match-api.herokuapp.com/"
-    : "http://192.168.1.20:4000";
+    ? "https://movie-match-api.herokuapp.com"
+    : "https://movie-match-api.herokuapp.com";
