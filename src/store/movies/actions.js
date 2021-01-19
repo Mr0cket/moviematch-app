92c477669baaa5b667773b5b17a917ee9fe6b88b
Milo Silva <milo.silva234@gmail.com>
2021-01-19 15:26:51 +0100

    Create routes for fetching matches & liked Movies

--- src/store/movies/actions.js
+++ src/store/movies/actions.js
@@ -1,10 +1,23 @@
 export const DISLIKED_MOVIE = "DISLIKED_MOVIE";
 export const LIKED_MOVIE = "LIKED_MOVIE";
 export const PARTY_LIKED_MOVIE = "PARTY_LIKED_MOVIE";
+export const FETCHED_MATCHES = "FETCHED_MATCHES";
+export const FETCHED_LIKED_MOVIES = "FETCHED_LIKED_MOVIES";
+
+import axios from "axios";
+import { apiUrl } from "../../config/constants";
 // initialise socket.io here...?
 import socket from "../socket";
 
 // noticing a lot of actions which are too similar..
+export const newMatchesList = (matches) => ({
+  type: FETCHED_MATCHES,
+  payload: matches,
+});
+export const newLikedList = (likedMovies) => ({
+  type: FETCHED_LIKED_MOVIES,
+  payload: likedMovies,
+});
 export const partyLiked = (movie) => ({
   type: PARTY_LIKED_MOVIE,
   payload: movie,
@@ -33,5 +46,19 @@ export const movieDisliked = (movie) => async (dispatch, getState) => {
   dispatch(dislikedMovie(movie.id));
 };
 export const sendMovieLiked = () => {};
+
+export const fetchMovieList = (type) => async (dispatch, getState) => {
+  console.log("type", type);
+  console.log("isMatches:", type === "matches");
+  const token = getState().user.token;
+  try {
+    const response = await axios.get(`${apiUrl}/movies/${type}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    dispatch(type === "matches" ? newMatchesList(response.data) : newLikedList(response.data));
+  } catch (error) {
+    console.log("error fetching Movies", error);
+  }
+};
 
 //socket events here??
--- src/store/movies/reducer.js
+++ src/store/movies/reducer.js
@@ -1,11 +1,14 @@
-import { FETCHED_MOVIES } from "../staging/actions";
+import { FETCHED_LIKED_MOVIES, FETCHED_MATCHES } from "./actions";
 
-const initialState = {};
+const initialState = { matches: [], liked: [] };
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case FETCHED_MOVIES: {
-      return state;
+    case FETCHED_MATCHES: {
+      return { ...state, matches: action.payload };
+    }
+    case FETCHED_LIKED_MOVIES: {
+      return { ...state, liked: action.payload };
     }
     default:
       return state;
