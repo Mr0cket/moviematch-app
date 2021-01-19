22cbb206bb63d4e8e2c1cb632fc3a2f15a7ae692
Milo Silva <milo.silva234@gmail.com>
2021-01-19 15:18:18 +0100

    Create a reusable screen container with MessageBox

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,10 +1,31 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { useSelector } from "react-redux";
+import styled from "styled-components";
+import { selectMessage } from "../store/appState/selectors";
 
 export default function Container({ children }) {
-  return <View>{props.children}</View>;
+  const message = useSelector(selectMessage);
+  const MessageBox = styled.View``;
+  const MessageText = styled.Text``;
+  return (
+    <View style={styles.container}>
+      <MessageBox>
+        <MessageText>{message && message}</MessageText>
+      </MessageBox>
+      {children}
+    </View>
+  );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: "#d4d4f7",
+    alignItems: "center",
+    overflow: "scroll",
+  },
+});
 
-function Ascreen() {
-  return <Container></Container>;
-}
+/* flex: 1;
+align-items: "center";
+overflow: "scroll"; */
