9c9a9dc90e3bc25e116d905acbb37eaf6e3d14f0
Milo Silva <milo.silva234@gmail.com>
2021-01-19 17:55:18 +0100

    switch to scrollable view screen container

--- src/screens/LikedMovies/index.js
+++ src/screens/LikedMovies/index.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
-import { View, Text } from "react-native";
+import { Text, ScrollView, StyleSheet } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import Container from "../../components/Container";
 import { fetchMovieList } from "../../store/movies/actions";
 import { selectlikedMovies } from "../../store/movies/selectors";
 import MovieRow from "../../components/MovieRow";
-
+import Button from "../../components/Button";
 export default function LikedMovies() {
   const dispatch = useDispatch();
   const likedMovies = useSelector(selectlikedMovies);
@@ -17,9 +17,16 @@ export default function LikedMovies() {
 
   const likedMoviesList = likedMovies.map((movie) => <MovieRow key={movie.id} movie={movie} />);
   return (
-    <Container style={{ overflow: "scroll" }}>
+    <ScrollView style={styles.container}>
       <Text>My liked Movies</Text>
       {likedMoviesList}
-    </Container>
+      <Button text="reload " onPress={() => dispatch(fetchMovieList("liked"))} />
+    </ScrollView>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: "#d4d4f7",
+  },
+});
