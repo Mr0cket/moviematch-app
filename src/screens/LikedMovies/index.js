f22e5941427afa3a6a128a06f834d37365a31b96
Milo Silva <milo.silva234@gmail.com>
2021-01-20 18:46:12 +0100

    Switched .map=> FlatList component to render lists - better performance, but noticing some strange behaviour on IOS

--- src/screens/LikedMovies/index.js
+++ src/screens/LikedMovies/index.js
@@ -1,27 +1,38 @@
 import React, { useEffect } from "react";
-import { Text, ScrollView, StyleSheet } from "react-native";
+import { ActivityIndicator, FlatList, StyleSheet, RefreshControl } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
-import Container from "../../components/Container";
 import { fetchMovieList } from "../../store/movies/actions";
 import { selectlikedMovies } from "../../store/movies/selectors";
 import MovieRow from "../../components/MovieRow";
-import Button from "../../components/Button";
+import { appLoading } from "../../store/appState/selectors";
+import Title from "../../components/Title";
+
 export default function LikedMovies() {
   const dispatch = useDispatch();
   const likedMovies = useSelector(selectlikedMovies);
-
+  const loading = useSelector(appLoading);
   useEffect(() => {
     // initial list request
     if (likedMovies.length < 1) dispatch(fetchMovieList("liked"));
   }, []);
 
-  const likedMoviesList = likedMovies.map((movie) => <MovieRow key={movie.id} movie={movie} />);
+  // const likedMoviesList = likedMovies.map((movie) => <MovieRow key={movie.id} movie={movie} />);
   return (
-    <ScrollView style={styles.container}>
-      <Text>My liked Movies</Text>
-      {likedMoviesList}
-      <Button text="reload " onPress={() => dispatch(fetchMovieList("liked"))} />
-    </ScrollView>
+    <FlatList
+      ListHeaderComponent={<Title>Liked Movies</Title>}
+      ListEmptyComponent={
+        <>
+          <Title>Your liked list is empty</Title>
+          <Title>like some movies to add to the list</Title>
+        </>
+      }
+      data={likedMovies}
+      renderItem={MovieRow}
+      keyExtractor={(item) => item.id.toString()}
+      refreshControl={
+        <RefreshControl refreshing={loading} onRefresh={() => dispatch(fetchMovieList("liked"))} />
+      }
+    />
   );
 }
 const styles = StyleSheet.create({
--- src/screens/Matches/index.js
+++ src/screens/Matches/index.js
@@ -1,25 +1,56 @@
 import React, { useEffect } from "react";
-import { View, Text } from "react-native";
+import { FlatList, View, Text, ActivityIndicator, RefreshControl } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import Container from "../../components/Container";
 import { fetchMovieList } from "../../store/movies/actions";
 import { selectMatches } from "../../store/movies/selectors";
 import MovieRow from "../../components/MovieRow";
+import styled from "styled-components";
+import { appLoading } from "../../store/appState/selectors";
 
 export default function Matches() {
   const dispatch = useDispatch();
   const matches = useSelector(selectMatches);
+  const loading = useSelector(appLoading);
 
   useEffect(() => {
     // initial list request
     if (matches.length < 1) dispatch(fetchMovieList("matches"));
   }, []);
+  const Title = styled.Text`
+    font-size: 18px;
+    font-weight: 700;
+    width: 100%;
+    text-align: center;
+    margin-bottom: 10%;
+  `;
 
-  const matchesList = matches.map((movie) => <MovieRow key={movie.id} movie={movie} />);
+  // const matchesList = matches.map((movie) => <MovieRow key={movie.id} movie={movie} />);
   return (
+    <FlatList
+      ListHeaderComponent={<Title>Party Matches</Title>}
+      ListEmptyComponent={
+        <>
+          <Title>Your Matches list is empty</Title>
+          <Title>Join a party to find matches</Title>
+        </>
+      }
+      data={matches}
+      renderItem={MovieRow}
+      keyExtractor={(item) => item.id.toString()}
+      refreshControl={
+        <RefreshControl
+          refreshing={loading}
+          onRefresh={() => dispatch(fetchMovieList("matches"))}
+        />
+      }
+    ></FlatList>
+  );
+
+  /* return (
     <Container>
       <Text>My Matches</Text>
       {matchesList}
     </Container>
-  );
+  ); */
 }
