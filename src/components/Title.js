f317e82178d88cdb9eedb810de0b312bcbcfe606
Milo Silva <milo.silva234@gmail.com>
2021-01-20 18:39:23 +0100

    Create styled component Title - create consistent titles across all screens

---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+import styled from "styled-components/native";
+const Title = styled.Text`
+  font-size: 26px;
+  font-weight: 700;
+  width: 100%;
+  text-align: center;
+  margin-bottom: 10%;
+`;
+export default Title;
