41d32ece0bcb382c1f9ca887cb24ddd0468d41fd
Milo Silva <milo.silva234@gmail.com>
2021-01-21 14:02:08 +0100

    added a selector to select list of users in party

--- src/store/user/selectors.js
+++ src/store/user/selectors.js
@@ -1,3 +1,4 @@
 export const selectToken = (state) => state.user.token;
 export const selectUser = (state) => state.user;
+export const selectParty = (state) => state.user.party;
 export const isAdmin = (state) => state.user.isAdmin;
