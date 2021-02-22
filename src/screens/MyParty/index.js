7099940ea0c2fb450780c3f3fd8645ef286de241
Milo Silva <milo.silva234@gmail.com>
2021-02-22 14:49:33 +0100

    Restyle Party page for consistency across app

--- src/screens/MyParty/index.js
+++ src/screens/MyParty/index.js
@@ -1,32 +1,29 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components/native";
-import Title from "../../components/Title";
+import SubTitle from "../../components/SubTitle";
 import Container from "../../components/Container";
 import UserItem from "./UserItem";
 import { selectParty } from "../../store/user/selectors";
 import SearchForUser from "./SearchForUser";
+import { StyleSheet, View } from "react-native";
 
-console.log(process.env);
 export default function index() {
   // const userSearchState = useSelector(selectUserSearch)
   const partyUsers = useSelector(selectParty);
   const partyList = partyUsers.map((user) => <UserItem key={user.id} user={user} />);
   return (
     <Container>
-      <Title>My party</Title>
-      <SubTitle>Users</SubTitle>
-      {partyList}
-      <SubTitle>Invite A Friend</SubTitle>
+      <SubTitle style={styles.subtitle}>Party Members {"  "}</SubTitle>
+      <View style={{ backgroundColor: "white", width: "95%", borderRadius: 8 }}>{partyList}</View>
+      <SubTitle style={styles.subtitle}>Invite A Friend {"  "}</SubTitle>
       <SearchForUser />
     </Container>
   );
 }
-
-const SubTitle = styled.Text`
-  font-size: 20px;
-  margin-bottom: 5%;
-  margin-top: 15%;
-  margin-left: 15%;
-  align-self: flex-start;
-`;
+const styles = StyleSheet.create({
+  subtitle: {
+    marginTop: 15,
+    marginLeft: 20,
+  },
+});
