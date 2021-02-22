7a534f14d272af55459244dcc8dfb67ba7a19127
Milo Silva <milo.silva234@gmail.com>
2021-02-22 14:50:23 +0100

    Restyle user search

--- src/screens/MyParty/SearchForUser.js
+++ src/screens/MyParty/SearchForUser.js
@@ -13,12 +13,9 @@ export default function SearchForUser() {
   const [searchString, setsearchString] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const searchForUser = async () => {
-    console.log("Implement the searchy stuff here");
-
     // Query BE for the search search results
     try {
       const res = await axios.get(`${apiUrl}/party/usersearch?searchString=${searchString}`);
-
       setSearchResults(res.data);
     } catch (e) {
       console.log("error :(");
@@ -26,21 +23,20 @@ export default function SearchForUser() {
       if (e.response) console.log(e.response.data);
     }
   };
-  console.log("searchResults:", searchResults);
   return (
     <View style={styles.container}>
-      <View style={{ flexDirection: "row" }}>
+      <View style={styles.inputContainer}>
         <TextInput
           value={searchString}
           style={styles.input}
           onChangeText={setsearchString}
           placeholder="name or email"
         />
-        <TouchableOpacity style={{ marginTop: 20, marginLeft: 10 }} onPress={searchForUser}>
+        <TouchableOpacity onPress={searchForUser}>
           <MaterialIcons name="person-search" size={38} color="black" />
         </TouchableOpacity>
       </View>
-      <View style={{ marginTop: "5%" }}>
+      <View style={styles.userList}>
         {searchResults.length > 0 &&
           searchResults.map((user) => <UserItem key={user.id} user={user} addList={true} />)}
       </View>
@@ -48,18 +44,26 @@ export default function SearchForUser() {
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
-    width: width * 0.8,
+    width: width,
   },
-  input: {
-    width: 200,
+  inputContainer: {
+    width: width * 0.95,
     height: 40,
     borderRadius: 10,
     backgroundColor: "#fff",
     marginTop: 15,
+    marginLeft: 10,
     paddingLeft: 10,
+    flexDirection: "row",
   },
-  userSearchList: {},
+  input: { width: "90%", height: "100%" },
+  userList: {
+    backgroundColor: "white",
+    width: "95%",
+    borderRadius: 8,
+    marginTop: "5%",
+    marginLeft: 10,
+  },
 });
