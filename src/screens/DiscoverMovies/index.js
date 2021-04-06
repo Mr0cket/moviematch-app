ea6aeaa06e3378163553d7ad93085022244d369f
Milo Silva <milo.silva234@gmail.com>
2021-04-06 15:35:23 +0200

    reconfigure like/dislike btns to call stackRef imperative handle

--- src/screens/DiscoverMovies/index.js
+++ src/screens/DiscoverMovies/index.js
@@ -24,62 +24,64 @@ const ButtonRow = styled.View`
   flex-direction: row;
 `;
 
-export default function index({ navigation, route }) {
-  const dispatch = useDispatch();
-  const modalMovie = useSelector(selectMatchModal);
-  const stagingLists = useSelector(selectStagingList, (prevList, CurrList) => JSON.stringify(prevList) === JSON.stringify(CurrList));
-  const [swipe, setSwipe] = useState({ dir: "", count: 0 });
-  const [isMounted, setMounted] = useState(false)
+export default function index ({ navigation, route }) {
+  const dispatch = useDispatch()
+  const modalMovie = useSelector(selectMatchModal)
+  const stagingLists = useSelector(selectStagingList, (prevList, CurrList) => JSON.stringify(prevList) === JSON.stringify(CurrList))
+  // const [isMounted, setMounted] = useState(false)
   useEffect(() => {
     // initial list request
+    console.log('staging', stagingLists.map(movie => movie.title))
     if (stagingLists.length < 2) dispatch(fetchStagingList(true))
-    else setMounted(true)
-  }, [stagingLists]);
+    // else setMounted(true)
+  }, [stagingLists])
   const handleLike = (movie) => {
-    dispatch(movieliked(movie));
-  };
+    dispatch(movieliked(movie))
+  }
 
   const handleDislike = (movie) => {
-    dispatch(movieDisliked(movie));
-  };
+    dispatch(movieDisliked(movie))
+  }
   const handleSwipe = (direction, movie) => {
-    console.log("[handleSwipe] direction:", direction);
+    console.log("[handleSwipe] direction:", direction)
     switch (direction) {
       case "RIGHT":
-        return handleLike(movie);
+        return handleLike(movie)
       case "LEFT":
-        return handleDislike(movie);
+        return handleDislike(movie)
     }
-  };
+  }
+  const stackRef = React.useRef()
+
   const onAllSwiped = () => {
-    setMounted(false)
+    // setMounted(false)
     dispatch(fetchStagingList(false))
   }
 
   if (stagingLists.length > 0) {
-    console.log("here");
     return (
       <Container>
         <View style={{ width: width / 1.17, height: height / 1.26 }}>
-          {isMounted &&
-            <MovieStack
-              listId={0}
-              onAllSwiped={onAllSwiped}
-              handleSwipe={handleSwipe}
-              navigation={navigation}
-              swipe={swipe}
-            />}
+          {/* {isMounted && */}
+          <MovieStack
+            ref={stackRef}
+            listId={0}
+            stagingList={stagingLists}
+            onAllSwiped={onAllSwiped}
+            handleSwipe={handleSwipe}
+            navigation={navigation}
+          />
         </View>
         <ButtonRow>
           <Button
             text={<Entypo name="thumbs-down" size={32} color="#f0ece3" />}
             style={{ backgroundColor: "#900d0d", borderColor: "#810000" }}
-            onPress={() => setSwipe({ dir: "LEFT", count: swipe.count++ })}
+            onPress={() => stackRef.current.imperativeSwipe("LEFT")}
           />
           <Button
             text={<Entypo name="thumbs-up" size={32} color="#f0ece3" />}
             style={{ backgroundColor: "#158467", borderColor: "#065446" }}
-            onPress={() => setSwipe({ dir: "RIGHT", count: swipe.count++ })}
+            onPress={() => stackRef.current.imperativeSwipe("RIGHT")}
           />
         </ButtonRow>
         {modalMovie && <MatchModal navigation={navigation} modalMovie={modalMovie} />}
